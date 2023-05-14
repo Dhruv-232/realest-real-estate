a4111e9fc5bd3a4205cad7f5291a84f9e3be2a56
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-14 11:04:13 +1000

    Connected mongodb successfully

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) .then(() => console.log("Connected successfully")) .catch((err) => { console.error(err)})
+
+app.listen(process.env.PORT, () => console.log("Server has been started successfully"))
