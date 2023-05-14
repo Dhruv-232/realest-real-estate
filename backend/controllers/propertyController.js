dd4a90303cdbdbfd372492137f1bbc8542ed2275
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-14 13:31:11 +1000

    Get individual property done

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -43,4 +43,30 @@ propertyController.get('/find', async (req, res) => {
     }
 })
 
-// get counts of types
+// get counts of different types of properties
+propertyController.get('/find/types', async (req, res) => {
+    try {
+        const beachType = await Property.countDocuments({ type: 'beach' })
+        const mountainType = await Property.countDocuments({ type: 'mountain' })
+        const villageType = await Property.countDocuments({ type: 'village' })
+
+        return res.status(200).json({ beach: beachType, mountain: mountainType, village: villageType })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
+
+// get individual property
+propertyController.get('/find/:id', async (req, res) => {
+    try {
+        const property = await Property.findById(req.params.id).populate('currentOwner', '-password')
+
+        if (!property) {
+            throw new Error('No such property with that id')
+        } else {
+            return res.status(200).json(property)
+        }
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
