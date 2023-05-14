7e86cb5a322ec1eff62f46674415b43ca708329f
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-14 11:51:37 +1000

    Worked on authController and fixed error in nodemon

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,13 +3,24 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
 
+// register
 authController.post('/register', async(req,res) => {
     try{
         const isExisting = await User.findOne({email: req.body.email})
         if (isExisting){
             throw new Error("Alread such an email exists")
         }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        const newUser = await User.create({...req.body, password: hashedPassword})
+
+        const {password, ...others} = newUser._doc
+        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '4h'})
+
+        return res.status(201).json({other,token})
     } catch(error) {
         return res.status(500).json(error.message)
     }
 })
+
+module.exports = authController;
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const authController = require('./controllers/authController')
 const app = express();
 
 // Connecting mongodb
