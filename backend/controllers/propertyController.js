c5835c89f8e515fdc9f1a765341b8569de841e51
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-14 13:12:09 +1000

    Verify token done

---
+++ backend/controllers/propertyController.js
@@ -0,0 +1,19 @@
+const Property = require('../models/Property')
+const propertyController = require('express').Router()
+const verifyToken = require("../middlewares/verifyToken")
+
+// get all 
+propertyController.get('/getAll', async(req, res) => {
+    try {
+        
+    }
+    catch (error) {
+
+    }
+})
+
+// get featured
+
+// get all from a specific type
+
+// get counts of types
