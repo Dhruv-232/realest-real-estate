9197203091ce5e59845b0f3e50f4c7084ca88c04
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-14 11:53:34 +1000

    Added the two lines of middleware from mini proj

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ mongoose.connect(process.env.MONGO_URL,  {
   }) .then(() => console.log("Connected successfully")) .catch((err) => { console.error(err)})
 
 // routes and middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use("/auth", authController)
 
 // Connecting server
