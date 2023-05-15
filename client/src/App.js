191a5e398029e2fc577fd4d3f10273fb6bf43a1d
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-15 22:51:47 +1000

    App.js routes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import FeaturedProperties from './components/featuredProperties/FeaturedProperties';
+import Footer from './components/footer/footer';
+import Newsletter from './components/newsletter/newsletter';
+import PopularProperties from './components/popularProperties/PopularProperties';
+import Properties from './components/properties/Properties';
+import PropertyDetail from './components/propertyDetail/PropertyDetail';
+import Signin from './components/signin/Signin';
+import Signup from './components/signup/Signup';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={
+          <>
+          <Navbar />
+          <Hero /> 
+          <PopularProperties />
+          <FeaturedProperties /> 
+          <Newsletter />
+          <Footer />
+          </>
+          
+        } />
+        
+        <Route path='/properties' element={
+        <>
+        <Navbar />
+        <Properties />
+        <Footer />
+        </>} />
+        <Route path='/propertyDetails/:id' element={
+          <>
+          <Navbar />
+          <PropertyDetail />
+          <Footer />
+          </>
+        } />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
     </div>
   );
 }
